784a51e24915c5a62fa9809f04b60e6097cbb45c
AriiMe <57349348+AriiMe@users.noreply.github.com>
2020-10-01 12:54:27 +0200

    Create dumbtest.js

---
+++ dumbtest.js
@@ -0,0 +1,17 @@
+/*
+const square = function (x) {
+	
+	const result = x * x;
+	
+	return result;
+};
+	console.log("The result of 4*4 is: " + square(4));
+	console.log("The result of 6*6 is: " + square(6));
+*/
+
+const square = function (x) {
+	const result = (x * x);
+	return result;
+};
+
+console.log(square(6));
